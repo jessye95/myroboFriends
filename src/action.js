e2a19b6ea7ba2530e8fdf85e7660842dff2f09a7
jessye95 <jessye95@gmail.com>
2019-01-04 23:35:14 +0000

    security fix

--- src/action.js
+++ src/action.js
@@ -4,7 +4,7 @@ import {
   REQUEST_ROBOTS_PENDING,
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED
-} from './constant.js'
+} from './constant'
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
