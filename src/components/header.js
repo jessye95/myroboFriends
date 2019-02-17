2cbfed57b13d63b3ad1419320e6ceda762bd6007
jessye95 <jessye95@gmail.com>
2019-02-17 18:31:27 +0000

    update header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import CounterButton from './counterButton'
+
+const headerStyle = {
+  fontSize: '3rem',
+  color: 'white',
+  fontFamily: "'Open sans', sans-serif",
+  fontWeight: 300,
+  textTransform: 'uppercase',
+  letterSpacing: '3px'
+}
 
 class Header extends React.Component {
   // shouldComponentUpdate (nextProps, nextState) {
@@ -8,8 +16,10 @@ class Header extends React.Component {
   render () {
     return (
       <div>
-        <h1 className='f1'>RoboFriends</h1>
-        <CounterButton />
+        <h1
+          className='f1'
+          style={headerStyle}>
+          RoboFriends</h1>
       </div>
     )
   }
